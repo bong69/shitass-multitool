2d33ebde9b7ad2d903a5058aafa30dbabc859df2
bong69 <unizzxx@gmail.com>
2021-10-29 20:51:18 -0400

    a whole ton of shit

--- shitass.py
+++ shitass.py
@@ -8,7 +8,7 @@ import concurrent.futures
 from os import system, name
 from random import randint
 from colorama import Fore
-cummands = ['1','2','3','4','5','6','clear','cls','logout','exit','quit']
+cummands = ['1','2','3','4','5','6','7','8','clear','cls','logout','exit','quit']
 
 def shitass_clear():
 	if name == 'nt':
@@ -18,6 +18,20 @@ def shitass_clear():
 
 def shitass_exit():
 	sys.exit("   ╠═[X] EXITING > GOODBYE.")
+
+def shitass_toucan(TOKEN):
+	headers = {
+		'Authorization': f'{TOKEN}'
+	}
+	poopies = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
+	try:
+		if poopies.status_code == 200:
+			fortnite = 0
+		else:
+			fortnite = 1
+	except Exception:
+		fortnite = 2
+	return fortnite
 
 def shitass_main():
 	global newprompt
@@ -38,29 +52,11 @@ def shitass_main():
 				╚══════╝╚═╝  ╚═╝╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
 																													                                               
 		═════════════════════════════════════════════════════════════════════════════════════
-		 [{Fore.CYAN}1{Fore.RESET}] IP/DOMAIN CHECKER | [{Fore.MAGENTA}2{Fore.RESET}] PORT SCANNER | [{Fore.CYAN}3{Fore.RESET}] TOKEN INFO | [{Fore.MAGENTA}4{Fore.RESET}] MULTI TOKEN CHECKER
-		 [{Fore.CYAN}5{Fore.RESET}] WEBHOOK SPAMMER   | [{Fore.MAGENTA}6{Fore.RESET}] PROXY GEN    | 
+		 [{Fore.CYAN}1{Fore.RESET}] IP/DOMAIN CHECKER | [{Fore.MAGENTA}2{Fore.RESET}] PORT SCANNER | [{Fore.CYAN}3{Fore.RESET}] PROXY GEN  | [{Fore.MAGENTA}4{Fore.RESET}] REVESE DNS  |
+		 [{Fore.CYAN}5{Fore.RESET}] DISCORD REPORTER  | [{Fore.MAGENTA}6{Fore.RESET}] WEBHOOK SPAM | [{Fore.CYAN}7{Fore.RESET}] TOKEN INFO | [{Fore.MAGENTA}8{Fore.RESET}] TOKEN CHECK |
 		═════════════════════════════════════════════════════════════════════════════════════''')
 
 
-def pscan(ipaddress, port, threads):
-	print_lock = threading.Lock()
-
-	def scan(ipaddress, port):
-		scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		scanner.settimeout(1)
-		try:
-			scanner.connect((ipaddress, port))
-			scanner.close()
-			with print_lock:
-				print()
-				print(f'[{Fore.WHITE}{port}{Fore.RESET}]' + f'{Fore.GREEN}OPEN{Fore.RESET}')
-		except:
-			pass
-
-	with concurrent.futures.ThreadPoolExecutor(max_workers=int(threads)) as executor:
-		for port in range(int(port)):
-			executor.submit(scan, ipaddress, port + 1)
 
 
 def ip(ipaddress):
@@ -71,7 +67,7 @@ def ip(ipaddress):
 	data = req.json()
 	status = data['status']
 	if(status == 'fail'):
-		input(f'{Fore.RED}REQUEST FAILED{Fore.RESET}')
+		input('   [❌] REQUEST FAILED')
 	else:
 		query = data['query']
 		continent = data['continent']
@@ -90,22 +86,172 @@ def ip(ipaddress):
 
 		print(f'''
 		[{Fore.MAGENTA}IP:{Fore.RESET}]                     {query}
-		[{Fore.MAGENTA}CONTINENT:{Fore.RESET}]              {continent}		
+		[{Fore.MAGENTA}CONTINENT:{Fore.RESET}]              {continent}
 		[{Fore.MAGENTA}COUNTRY:{Fore.RESET}]		  {country}
 		[{Fore.MAGENTA}REGION:{Fore.RESET}]		  {region}
 		[{Fore.MAGENTA}CITY:{Fore.RESET}]			  {city}
 		[{Fore.MAGENTA}ZIP:{Fore.RESET}]			  {zipp}
 		[{Fore.MAGENTA}LATITUDE:{Fore.RESET}]		  {lat}
 		[{Fore.MAGENTA}LONGITUDE:{Fore.RESET}]		  {lon}
-		[{Fore.MAGENTA}ISP:{Fore.RESET}]		          {isp} 
-		[{Fore.MAGENTA}ORGINIZATION:{Fore.RESET}]	          {org} 	
-		[{Fore.MAGENTA}AS:{Fore.RESET}]		          {asn}     
-		[{Fore.MAGENTA}MOBILE?:{Fore.RESET}]		  {mobile} 
-		[{Fore.MAGENTA}PROXY?:{Fore.RESET}]		  {proxy} 
-		[{Fore.MAGENTA}HOSTING?:{Fore.RESET}]		  {hosting} 
+		[{Fore.MAGENTA}ISP:{Fore.RESET}]		          {isp}
+		[{Fore.MAGENTA}ORGINIZATION:{Fore.RESET}]	          {org}
+		[{Fore.MAGENTA}AS:{Fore.RESET}]		          {asn}
+		[{Fore.MAGENTA}MOBILE?:{Fore.RESET}]		  {mobile}
+		[{Fore.MAGENTA}PROXY?:{Fore.RESET}]		  {proxy}
+		[{Fore.MAGENTA}HOSTING?:{Fore.RESET}]		  {hosting}
 
 
 		''')
+
+
+def pscan(ipaddress, port, threads):
+	shitass_main()
+	print_lock = threading.Lock()
+
+	def scan(ipaddress, port):
+		scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		scanner.settimeout(1)
+		try:
+			scanner.connect((ipaddress, port))
+			scanner.close()
+			with print_lock:
+				print()
+				print(f'   [{Fore.WHITE}{port}{Fore.RESET}]' + f'{Fore.GREEN}OPEN{Fore.RESET}')
+		except:
+			pass
+
+	with concurrent.futures.ThreadPoolExecutor(max_workers=int(threads)) as executor:
+		for port in range(int(port)):
+			executor.submit(scan, ipaddress, port + 1)
+
+
+def proxygen(protocol):
+	shitass_main()
+
+	if protocol == 1:
+		url = f'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
+	elif protocol == 2:
+		url = f'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all'
+	elif protocol == 3:
+		url = f'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all'
+	elif protocol == 4:
+		url = f'https://api.proxyscrape.com/v2/?request=getproxies&protocol=all&timeout=10000&country=all&ssl=all&anonymity=all'
+	else:
+		print(f'   [❗] {Fore.RED}INVALID PROTOCOL{Fore.RESET}')
+		return
+
+
+	req = requests.get(url)
+	data = req.text
+	if req.status_code == 200:
+		balls = randint(1,999)
+		name = f'{protocol.upper()}_PROXIES{balls}'
+		with open(f'{name}.txt', 'w') as saveFile:
+			saveFile.write(data)
+
+		with open(f'{name}.txt') as poop:
+			lines = poop.readlines()
+
+		with open(f'{name}.txt', 'r+') as poop:
+			lines = filter(lambda x: x.strip(), lines)
+			poop.writelines(lines)
+
+		print(f'  [✔️] SAVED PROXIES TO {name}')
+	else:
+		print('   [❌]REQUEST FAILED')
+
+
+def reversedns(ipaddy):
+	shitass_main()
+	try:
+		domain = socket.gethostbyaddr(ipaddy)[0]
+		print(f'   [✔️] DOMAIN FOUND: {domain}')
+	except:
+		print('   [❌] REVERSE DNS FAILED')
+
+
+def discreporter():
+	shitass_main()
+	penis = True
+	print(f'   [❗] {Fore.RED}WARNING{Fore.RESET}{Fore.WHITE} DO NOT USE YOUR MAIN DISCORD TOKEN FOR THIS YOU WILL PROBABLY GET {Fore.RESET}{Fore.RED}BANNED{Fore.RESET}')
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER DISCORD TOKEN', end=''); TOKEN = input('  :  ')
+
+
+	fortnite = shitass_toucan(TOKEN)
+	if fortnite == 0:
+		shitass_main()
+		print('   [✔️] VALID TOKEN PROVIDED')
+	elif fortnite == 1:
+		print('   [❌] INALID TOKEN PROVIDED')
+		return
+	else:
+		print('   [❌] UNABLE TO CHECK TOKEN')
+		return
+
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER SERVER ID', end=''); fart_serverid = input('  :  ')
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER CHANNEL ID', end=''); fart_channelid = input('  :  ')
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER MESSAGE ID', end=''); fart_messageid = input('  :  ')
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER DELAY IN SECONDS', end=''); fart_delay = input('  :  ')
+
+	shitass_main()
+	print(f'   ╔════════════[{Fore.MAGENTA}<>{Fore.RESET}]\n   ╚═══>{Fore.CYAN}Shitass{Fore.RESET}@bignuts {Fore.MAGENTA}-{Fore.RESET} ')
+
+	print(f'   [{Fore.MAGENTA}>{Fore.RESET}] CHOOSE REPORT REASON\n   [{Fore.MAGENTA}>{Fore.RESET}] 0 - ILLEGAL CONTENT\n   [{Fore.MAGENTA}>{Fore.RESET}] 1 - HARASSMENT\n   [{Fore.MAGENTA}>{Fore.RESET}] 2 - SPAM OR PHISHING LINKS\n   [{Fore.MAGENTA}>{Fore.RESET}] 3 - SELF-HARM\n   [{Fore.MAGENTA}>{Fore.RESET}] 4 - NSFW CONTENT\n', end=''); fart_reason = input(f'CHOICE [{Fore.MAGENTA}>{Fore.RESET}]:')
+
+
+	while penis == True:
+		try:
+			shitass_main()
+			queef = requests.post(
+				'https://discordapp.com/api/v9/report', json={
+					'channel_id': fart_channelid,
+					'message_id': fart_messageid,
+					'guild_id': fart_serverid,
+					'reason': fart_reason
+				}, headers={
+					'Accept': '*/*',
+                	'Accept-Encoding': 'gzip, deflate',
+                	'Accept-Language': 'sv-SE',
+                	'User-Agent': 'Discord/21295 CFNetwork/1128.0.1 Darwin/19.6.0',
+                	'Content-Type': 'application/json',
+                	'Authorization': TOKEN
+				}
+			)
+			if (status := queef.status_code) == 201:
+				print('   [✔️] REPORT SENT')
+			elif status == 401:
+				print('   [❌] ERROR 401 UNAUTHORIZED')
+			else:
+				print('   [❌] REPORT NOT SENT')
+		except Exception as e:
+			print(e)
+
+		time.sleep(int(fart_delay))
+
+
+def webhookfuck(link, name, message, amount):
+	shitass_main()
+	sex = True
+	shitter = 0
+
+	while sex == True if amount == "inf" else shitter < int(amount):
+		try:
+			req = requests.post(
+				link,
+				json={
+					"content": str(message),
+					"username": str(name),
+					"avatar_url": "https://i.kym-cdn.com/entries/icons/original/000/018/166/pakalu.png" # change this shit if u want
+				}
+			)
+			if req.status_code == 204:
+				print('   [✔️] SENT MESSAGE')
+			else:
+				print('   [❌] FAILED TO SEND MESSAGE')
+		except:
+			sex = False
+			print(f'   {Fore.RED}STOPPED{Fore.RESET}')
+		shitter += 1
 
 
 def tokenInfo(token):
@@ -132,29 +278,29 @@ def tokenInfo(token):
 
 			''')
 	else:
-		print(f'{Fore.RED}{token} IS INVALID{Fore.RESET}')
+		print('[❌] PROVIDED TOKEN IS INVALID')
 
 
-def tokenCheck():
+def tokencheck():
 	shitass_main()
-	input(f"[{Fore.GREEN}!{Fore.RESET}] PROGRAM WILL {Fore.RED}BREAK{Fore.RESET} WITHOUT A {Fore.GREEN}TOKENS.TXT{Fore.RESET}")
+	input(f"   [❗] PROGRAM WILL {Fore.RED}BREAK{Fore.RESET} WITHOUT A {Fore.GREEN}TOKENS.TXT{Fore.RESET}")
 	checked = []
 	with open('tokens.txt', 'r') as f:
 		for line in f:
 			time.sleep(0)
 			token = line.rstrip("\n")
 			headers = {
-				'Authorization': f'{token}'  
+				'Authorization': f'{token}'
 			}
 			src = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
 			try:
 				if src.status_code == 200:
-					print(f"[{Fore.GREEN}VALID TOKEN{Fore.RESET}] > " + token)
+					print("[✔️] VALID TOKEN > " + token)
 					checked.append(token)
 				else:
-					print(f"[{Fore.RED}INVALID TOKEN{Fore.RESET}] > " + token)
+					print("[❌] INVALID TOKEN > " + token)
 			except Exception:
-				print (f"TOKEN-CHECKER [{Fore.RED}FAILED{Fore.RESET}] | CHECK INTERNET CONNECTIVITY, OR TRY AGAIN LATER")
+				print (f"[{Fore.RED}X{Fore.RESET}] TOKEN CHECK FAILED")
 	if len(checked) > 0:
 		save = input(f'{Fore.GREEN}{len(checked)} VALID TOKENS{Fore.RESET}\nSAVE TOKENS TO FILE? (Y/N)')
 		if save == 'y':
@@ -163,66 +309,6 @@ def tokenCheck():
 			with open(f'{name}.txt', 'w') as saveFile:
 				saveFile.write('\n'.join(checked))
 			print(f'SAVED TOKENS TO {name}')
-
-
-def webhookfuck(link, name, message, amount):
-	shitass_main()
-	sex = True
-	shitter = 0
-
-	while sex == True if amount == "inf" else shitter < int(amount):
-		try:
-			req = requests.post(
-				link,
-				json={
-					"content": str(message),
-					"username": str(name),
-					"avatar_url": "https://i.kym-cdn.com/entries/icons/original/000/018/166/pakalu.png" # change this shit if u want
-				}
-			)
-			if req.status_code == 204:
-				print(f'{Fore.GREEN}[SENT MESSAGE]{Fore.RESET}')
-			else:
-				print(f'{Fore.RED}[FAILED TO SEND MESSAGE]{Fore.RESET}')
-		except:
-			sex = False
-			print(f'{Fore.RED}STOPPED{Fore.RESET}')
-		shitter += 1
-
-
-def proxygen(protocol):
-	shitass_main()
-
-	if protocol == 1:
-		protocol = 'http'
-	elif protocol == 2:
-		protocol = 'socks4'
-	elif protocol == 3:
-		protocol = 'socks5'
-	else:
-		protocol = 'all'
-		
-	url = f'https://api.proxyscrape.com/v2/?request=getproxies&protocol={protocol}&timeout=10000&country=all&ssl=all&anonymity=all'
-
-	req = requests.get(url)
-	data = req.text
-	if req.status_code == 200:
-		balls = randint(1,999)
-		name = f'{protocol.upper()}_PROXIES{balls}'
-		with open(f'{name}.txt', 'w') as saveFile:
-			saveFile.write(data)
-
-		with open(f'{name}.txt') as poop:
-			lines = poop.readlines()
-		
-		with open(f'{name}.txt', 'r+') as poop:
-			lines = filter(lambda x: x.strip(), lines)
-			poop.writelines(lines)
-					
-		print(f'{Fore.GREEN}SAVED PROXIES TO {name}{Fore.RESET}')
-	else:
-		print(f'{Fore.RED}REQUEST FAILED{Fore.RESET}')
-
 
 
 def cmds():
@@ -239,27 +325,32 @@ def cmds():
 			f"   ╠═[{Fore.MAGENTA}X{Fore.RESET}] {Fore.RED}Invalid{Fore.RESET} Option")
 		cmds()
 	if command == "1":
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER IP/DOMAIN', end=''); ipaddress = input('  :  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER IP/DOMAIN', end=''); ipaddress = input(':  ')
 		ip(ipaddress)
 	elif command == "2":
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER IP/DOMAIN', end=''); ipaddress = input('  :  ')
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER PORT RANGE EX: 1000', end=''); port = input('  :  ')
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER THREAD COUNT', end=''); threads = input('  :  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER IP/DOMAIN', end=''); ipaddress = input(':  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER PORT RANGE EX: 1000', end=''); port = input(':  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER THREAD COUNT', end=''); threads = input(':  ')
 		pscan(ipaddress, port, threads)
 	elif command == "3":
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER ACCOUNT TOKEN', end=''); token = input('  :  ')
-		tokenInfo(token)
-	elif command == "4":
-		tokenCheck()
-	elif command == "5":
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER WEBHOOK LINK', end=''); link = input('  :  ')
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER CUSTOM WEBHOOK NAME', end=''); name = input('  :  ')
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER MESSAGE', end=''); message = input('  :  ')
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER NUMBER OF MESSAGES PUT "inf" FOR INFINITE', end=''); amount = input('  :  ')
-		webhookfuck(link, name, message, amount)
-	elif command == "6":
-		print(f'[{Fore.MAGENTA}>{Fore.RESET}] ENTER PROXY PROTOCOL\n[{Fore.MAGENTA}>{Fore.RESET}] 1 FOR HTTPS\n[{Fore.MAGENTA}>{Fore.RESET}] 2 FOR SOCKS4\n[{Fore.MAGENTA}>{Fore.RESET}] 3 FOR SOCKS5\n[{Fore.MAGENTA}>{Fore.RESET}] 4 FOR ALL', end=''); protocol = input('  :  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] CHOOSE PROXY PROTOCOL\n   [{Fore.MAGENTA}>{Fore.RESET}] 1 - HTTPS\n   [{Fore.MAGENTA}>{Fore.RESET}] 2 - SOCKS4\n   [{Fore.MAGENTA}>{Fore.RESET}] 3 - SOCKS5\n   [{Fore.MAGENTA}>{Fore.RESET}] 4 - ALL\n', end=''); protocol = input(f'CHOICE [{Fore.MAGENTA}>{Fore.RESET}]:')
 		proxygen(protocol)
+	elif command == "4":
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER IP', end=''); ipaddy = input(':  ')
+		reversedns(ipaddy)
+	elif command == "5":
+		discreporter()
+	elif command == "6":
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER WEBHOOK LINK', end=''); link = input(':  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER CUSTOM WEBHOOK NAME', end=''); name = input(':  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER MESSAGE', end=''); message = input(':  ')
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER NUMBER OF MESSAGES PUT "inf" FOR INFINITE', end=''); amount = input(':  ')
+		webhookfuck(link, name, message, amount)
+	elif command == "7":
+		print(f'   [{Fore.MAGENTA}>{Fore.RESET}] ENTER ACCOUNT TOKEN', end=''); token = input(':  ')
+		tokenInfo(token)
+	elif command == "8":
+		tokencheck()
 	elif command == "clear":
 		shitass_main()
 	elif command == "cls":
@@ -279,4 +370,3 @@ while True:
 	except KeyboardInterrupt:
 		pass
 		print('')
-		
